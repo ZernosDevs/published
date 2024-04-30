fc4b2dbf712d530bcc85ef5c4da0d2fb4352a03c
Robin Park <parkkanidon@gmail.com>
2024-04-30 22:48:56 +0900

    finishing touches

--- src/components/NeuronSVGSmall.js
+++ src/components/NeuronSVGSmall.js
@@ -3,7 +3,7 @@ const NewSvgComponent = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
-    viewBox="370 200 1217.67 1685"
+    viewBox="370 400 1217.67 1685"
     {...props}
   >
     <defs>
